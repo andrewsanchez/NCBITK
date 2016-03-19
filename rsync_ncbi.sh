f1a2eb2d9bca8eb4cd3d5709c159511ab1bef5b9
Andrew Sanchez <heartsayer@gmail.com>
2016-03-19 16:27:29 -0700

    only get .fna.gz; ignore all_assemblies_versions

--- rsync_ncbi.sh
+++ rsync_ncbi.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-# Run as sudo
-# include="rsync_include.txt"
-# exclude="rsync_exclude.txt"
-organism="Acinetobacter_nosocomialis" # set to "*" for all bacteria
+# run as sudo
+
+organism="Escherichia_coli" # set this to just "*" for all bacteria
 location="bacteria/"$organism"/latest_assembly_versions/"
-directory="/home/truthling/MGGen/rsync_Acinetobacter_nosocomialis/" # will be created if doesn't already exist
+directory="/home/truthling/MGGen/"$organism"" # must be relative; will be created if doesn't already exist
 files=".fna.gz"
-renamescript="/home/truthling/MGGen/NCBI_tools/rename.python"
 
 # figure out which folders to exclude, e.g. unplaced_scaffolds, etc.
 
-rsync -iPrLtm -f="+ *"$files"" -f="+ */" -f="- *" ftp.ncbi.nlm.nih.gov::genomes/genbank/"$location" "$directory"
-
-# these two below produce the same results as the one above
-# rsync -iPrLtm -f="+ *"$files"" -f="+ */" -f="- *" ftp.ncbi.nlm.nih.gov::genomes/genbank/"$location" $directory
-# rsync -iPrLtm -f="+ *"$files"" -f="+ */" -f="- *" -f="- all_assembly_versions" ftp.ncbi.nlm.nih.gov::genomes/genbank/"$location" "$directory"
-
-# cp only the files to separate directory for renaming
-# Make sure this is only running once, after all files are downloaded.
-
-find "$directory" -type f -exec cp -t '/home/truthling/MGGen/renamed/' -- {} +
-
-python3 "$renamescript"
-
-# add line for decompressing files
+rsync -iPrLtm -f="+ *"$files"" -f="+ */" -f="- *" -f="- all_assembly_versions" ftp.ncbi.nlm.nih.gov::genomes/genbank/"$location" "$directory"
 
 # --exclude-from=FILE     read exclude patterns from FILE
 # --include-from=FILE     read include patterns from FILE
@@ -35,3 +20,7 @@ python3 "$renamescript"
 # -L, --copy-links            transform symlink into referent file/dir
 # -t, --times                 preserve modification times
 # -m, --prune-empty-dirs      prune empty directory chains from file-list
+
+mkdir -p "$directory"_local/
+sudo find "$directory" -type f -exec cp -t ""$directory"_local/" -- {} +
+
