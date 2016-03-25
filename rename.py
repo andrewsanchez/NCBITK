cd127c8e8da043f22f3a0865f002b518be9e14d9
Andrew Sanchez <heartsayer@gmail.com>
2016-03-25 12:25:44 -0700

    Sync, copy, and rename, all in one

--- rename.py
+++ rename.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python
+
 import pandas as pd
 import os
 import sys
+import subprocess
 
-summary = '/home/truthling/MGGen/assembly_summary.txt'
+just_fastas = sys.argv[1]
+summary = sys.argv[2]
 df = pd.read_csv(summary, delimiter='\t', index_col=0)
-copiedfiles = sys.argv[1]
 
 df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].isnull())].fillna('NA'))
 df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].notnull())].fillna(df['isolate']))
-negate_list = ' '
-df.organism_name.replace({'[^\w'+negate_list+']': ''}, regex=True, inplace=True)
-df.infraspecific_name.replace({'[ =]': '_'}, regex=True, inplace=True)
+df.organism_name.replace({' ': '_'}, regex=True, inplace=True)
+df.organism_name.replace({'[\W]': ''}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[ =\-\;]': '_'}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[\W]': ''}, regex=True, inplace=True)
 df.assembly_level.replace({' ': '_'}, regex=True, inplace=True)
 
-for f in os.listdir(copiedfiles):
+for f in os.listdir(just_fastas):
     if f.startswith('GCA'):
         id = (f.split('_')[0:2])
         id = ('_'.join(id))
@@ -23,7 +27,9 @@ for f in os.listdir(copiedfiles):
             org_name = df.get_value(id, 'organism_name')
             strain = df.get_value(id, 'infraspecific_name').strip('strain=')
             assembly_level  = df.get_value(id, 'assembly_level')
-            newname = '{}_{}_{}_{}.fasta'.format(id, org_name, strain, assembly_level)
-            old = copiedfiles+f
-            new = copiedfiles+newname
+            newname = '{}_{}{}_{}.fna.gz'.format(id, org_name, strain, assembly_level)
+            old = just_fastas+f
+            new = just_fastas+newname
             os.rename(old, new)
+
+subprocess.run(['gunzip', '-f', '/home/truthling/MGGen/Acinetobacter_nosocomialis_fastas/*.fna.gz'])
--- rsync_ncbi.py
+++ rsync_ncbi.py
@@ -3,26 +3,42 @@
 import sys
 import subprocess
 import os
+import pandas as pd
 
 organism = sys.argv[1]
 local_mirror = sys.argv[2]
 ftp_directory = 'bacteria/' + organism + '/latest_assembly_versions/'
-just_fastas = local_mirror + '_local'
+just_fastas = local_mirror + '_fastas/'
 
-subprocess.run(['rsync',
-                '-iPrLtmn',
-                '-f="- **unplaced_scaffolds**"',
-                '-f="+ *.fna.gz"',
-                '-f="+ */"',
-                '-f="- *"',
-                'ftp.ncbi.nlm.nih.gov::genomes/genbank/' + ftp_directory,
-                local_mirror,])
+if os.path.isdir(organism):
+    subprocess.run(['rsync',
+                    '-iPrLtm',
+                    '--exclude=**/unplaced_scaffolds/**',
+                    '-f=+ *.fna.gz',
+                    '-f=+ */',
+                    '--exclude=*',
+                    'ftp.ncbi.nlm.nih.gov::genomes/genbank/' + ftp_directory,
+                    '--log-file=log.txt',
+                    local_mirror])
 
+else:
+    os.mkdir(organism)
+    subprocess.run(['rsync',
+                    '-iPrLtm',
+                    '--exclude=**/unplaced_scaffolds/**',
+                    '-f=+ *.fna.gz',
+                    '-f=+ */',
+                    '--exclude=*',
+                    'ftp.ncbi.nlm.nih.gov::genomes/genbank/' + ftp_directory,
+                    '--log-file=log.txt',
+                    local_mirror])
+
+# copy just fastas from local NCBI mirror to just_fastas directory
 if os.path.isdir(just_fastas):
-    subprocess.run(['find', organism, '-type', 'f',
-                '-exec', 'cp',
-                '-t', just_fastas,
-                '-- {}', '+'])
+    subprocess.run(['sudo', 'find', organism, '-type', 'f',
+                    '-exec', 'cp',
+                    '-t', just_fastas,
+                    '-- {}', '+'])
 
 else:
     os.mkdir(just_fastas)
@@ -31,4 +47,32 @@ else:
                 '-t', just_fastas,
                 '-- {}', '+'])
 
-# sudo find "$ftpmirror" -type f -exec cp -t "$justfastas" -- {} +
+# Rename files in just_fastas directory
+summary = sys.argv[3]
+df = pd.read_csv(summary, delimiter='\t', index_col=0)
+
+df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].isnull())].fillna('NA'))
+df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].notnull())].fillna(df['isolate']))
+df.organism_name.replace({' ': '_'}, regex=True, inplace=True)
+df.organism_name.replace({'[\W]': ''}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[ =\-\;]': '_'}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[\W]': ''}, regex=True, inplace=True)
+df.assembly_level.replace({' ': '_'}, regex=True, inplace=True)
+
+for f in os.listdir(just_fastas):
+    if f.startswith('GCA'):
+        id = (f.split('_')[0:2])
+        id = ('_'.join(id))
+
+    for row in df.index:
+        if id == row:
+            org_name = df.get_value(id, 'organism_name')
+            strain = df.get_value(id, 'infraspecific_name').strip('strain=')
+            assembly_level  = df.get_value(id, 'assembly_level')
+            newname = '{}_{}{}_{}.fna.gz'.format(id, org_name, strain, assembly_level)
+            old = just_fastas+f
+            new = just_fastas+newname
+            os.rename(old, new)
+
+# Decompress files in just_fastas directory
+subprocess.run(['pigz', '-d', just_fastas+'*.fna.gz'])
