7d25297a358d4642c701049155a37d4551afd0d3
Andrew Sanchez <heartsayer@gmail.com>
2016-04-03 22:02:04 -0700

    add renaming function

--- pysync.py
+++ pysync.py
@@ -3,12 +3,24 @@
 import sys
 import subprocess
 import os
+import pandas as pd
 from ftplib import FTP
 
 ftp_site = 'ftp.ncbi.nlm.nih.gov'
 ftp = FTP(ftp_site)
 ftp.login()
 ftp.cwd('genomes/genbank/bacteria')
+
+summary = 'assembly_summary.txt' # location of assembly_summary.txt
+df = pd.read_csv(summary, delimiter='\t', index_col=0)
+
+df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].isnull())].fillna('NA'))
+df.update(df['infraspecific_name'][(df['infraspecific_name'].isnull()) & (df['isolate'].notnull())].fillna(df['isolate']))
+df.organism_name.replace({' ': '_'}, regex=True, inplace=True)
+df.organism_name.replace({'[\W]': ''}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[ =\-\;]': '_'}, regex=True, inplace=True)
+df.infraspecific_name.replace({'[\W]': ''}, regex=True, inplace=True)
+df.assembly_level.replace({' ': '_'}, regex=True, inplace=True)
 
 local_mirror = 'genbank_bacteria/'
 all_fastas = local_mirror.strip('/') + '_fastas/'
@@ -23,7 +35,7 @@ dirs = ftp.nlst()
 for organism in dirs[0:100]:
     print(str(dirs.index(organism))+ ' out of ' + str(len(dirs)))
 
-    just_fastas = all_fastas + organism
+    just_fastas = all_fastas + organism + '/'
     subprocess.run(['rsync',
                     '-iPrLt',
                     '-f=+ GCA*fna.gz',
@@ -56,3 +68,17 @@ for organism in dirs[0:100]:
                         '-exec', 'pigz',
                         '-d',
                         '-- {}', '+'])
+
+    for f in os.listdir(just_fastas):
+        id = (f.split('_')[0:2])
+        id = ('_'.join(id))
+
+        for row in df.index:
+            if id == row:
+                org_name = df.get_value(id, 'organism_name')
+                strain = df.get_value(id, 'infraspecific_name').strip('strain=')
+                assembly_level  = df.get_value(id, 'assembly_level')
+                newname = '{}_{}{}_{}.fna'.format(id, org_name, strain, assembly_level)
+                old = just_fastas+f
+                new = just_fastas+newname
+                os.rename(old, new)
