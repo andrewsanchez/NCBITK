2b0f16419c7db485d5c12c73bb211191a7774008
Andrew Sanchez <heartsayer@gmail.com>
2016-04-04 10:59:14 -0700

    update troubleshooting script

--- troubleshoot_rename.py
+++ troubleshoot_rename.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import pandas as pd
 import os
 import sys
 
@@ -8,43 +7,36 @@ just_fastas = sys.argv[1] # *_just_fastas
 summary = sys.argv[2] # assembly_summary.txt
 df = pd.read_csv(summary, delimiter='\t', index_col=0)
 
-fna_files = []
+total_fastas = []
 correctly_named = []
-incorrectly_named = []
+misnamed = []
 
-for f in os.listdir(just_fastas):
-    fna_files.append(f)
-    abs_path = just_fastas + f
-    fasta = open(abs_path, 'r')
-    line = fasta.readline()
-    organism = f.split('_')[2:3]
-    organism = '_'.join(organism)
-    if organism in line:
-        correctly_named.append(f)
-    else:
-        incorrectly_named.append(f)
-    fasta.close()
+for root, dirs, files in os.walk('tmp/genbank_bacteria_fastas/'):
+    for dir in dirs:
+      genus = dir.split('_')[0]
+    for name in files:
+        total_fastas.append(name)
+        abs_path = os.path.join(root, name)
+        fasta = open(abs_path, 'r')
+        line = fasta.readline()
+        species = name.split('_')[3:4]
+        species = '_'.join(species)
+        if genus in line:
+            correctly_named.append(name)
+            fasta.close()
+        else:
+            file = os.path.join(root, name)
+            misnamed.append(file)
+            fasta.close()
 
-print('Total number of files:  {}'.format(len(fna_files)))
+print('Total number of files:  {}'.format(len(total_fastas)))
 print('Correctly named files:  {}'.format(len(correctly_named)))
-print('Incorrectly named files:  {}'.format(len(incorrectly_named)))
+print('Misnamed files:  {}'.format(len(misnamed)))
+print('Misnamed files:')
 print('\n')
 
-for f in incorrectly_named:
-    abs_path = just_fastas + f
-    fasta = open(abs_path, 'r')
-    line = fasta.readline()
-    print(line)
-
-    #organism = f.split('_')[2:3]
-    #organism = '_'.join(organism)
-    #print(organism + '<- Filename')
-    print(f)
-
-    id = (f.split('_')[0:2])
-    id = ('_'.join(id))
-    for row in df.index:
-        if id == row:
-            print(df.get_value(id, 'organism_name'))
-
-print('\n')
+for f in misnamed:
+    with open(f, 'r') as fasta:
+        line = fasta.readline()
+        print(f)
+        print(line)
