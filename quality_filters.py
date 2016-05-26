7ba0638d3d0f0044a9d40a29ee63d9ca8d4e86e1
Andrew Sanchez <aas229@nau.edu>
2016-05-26 15:23:19 -0700

    fix looping problems and improve speed

--- quality_filters.py
+++ quality_filters.py
@@ -11,24 +11,41 @@ def quality_control(fasta_dir):
 
         file_names = []
         file_sizes = []
-        total_contigs = []
-        contig_lengths_totals = []
-        percent_N_totals = []
+        contig_totals = []
+        lengths = []
+        N_percent_totals = []
 
         for f in files:
-
             fasta = (os.path.join(root, f))
             file_names.append(f)
             file_sizes.append(os.path.getsize(fasta))
-            contigs = [seq.seq for seq in SeqIO.parse(fasta, "fasta")]
-            total_contigs.append(len(contigs))
-            contig_lengths = [len(str(seq.seq)) for seq in SeqIO.parse(fasta, "fasta")]
-            contig_lengths_totals.append(sum(contig_lengths))
-            percent_Ns = [float(str(seq.seq).count("N"))/float(len(str(seq.seq).upper()))*100 for seq in SeqIO.parse(fasta, "fasta")]
-            percent_N_totals.append("%.2f" % sum(percent_Ns))
 
-        SeqDataSet = list(zip(file_names, file_sizes, total_contigs, contig_lengths_totals, percent_N_totals))
-        seq_df = pd.DataFrame(data = SeqDataSet, columns=["Accession", "File Size", "Contigs", "Length", "% N"])
+            # Read all contigs for this fasta into a list
+            contigs = [ seq.seq for seq in SeqIO.parse(fasta, "fasta") ]
+
+            # Append the total number of contigs to contig_totals
+            contig_totals.append(len(contigs))
+
+            # Create a list to hold the length of each contig
+            # Append the total to lengths
+            contig_lengths = [ len(str(seq)) for seq in contigs ]
+            lengths.append(sum(contig_lengths)
+
+            # Create a list to hold the N count for each contig
+            # Append the total percent N to N_percent_totals
+            N_count = [ float(str(seq)).count("N") for seq in contigs ]
+            N_percent = sum(N_count)/float(total_length)
+            N_percent_totals.append(N_percent)
+
+           #contigs = [seq.seq for seq in SeqIO.parse(fasta, "fasta")]
+           #total_contigs.append(len(contigs))
+           #contig_lengths = [len(str(seq.seq)) for seq in SeqIO.parse(fasta, "fasta")]
+           #contig_lengths_totals.append(sum(contig_lengths))
+           #total_Ns = [float(str(seq.seq).count("N")) for seq in SeqIO.parse(fasta, "fasta")]
+           #percent_N_totals.append("%.1f" % sum(total_Ns)/sum(contig_lengths))
+
+        SeqDataSet = list(zip(file_names, file_sizes, contig_totals, lengths, N_percent_totals))
+        seq_df = pd.DataFrame(data = SeqDataSet, columns=["Accession", "File Size", "Contigs", "Total Length", "% N"])
         seq_df.to_csv(os.path.join(root, "stats.csv"), index=False)
 
 #def save_stats():
