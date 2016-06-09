efb8f475396d3f2ee28c6dac476e90621e1eb116
Andrew Sanchez ~ Student Wage - Temporary <aas229@wind.nauhpc>
2016-06-08 18:45:38 -0700

    add functions

--- df_queries.py
+++ df_queries.py
@@ -8,20 +8,31 @@ def read_csv(organism):
     df = pd.read_csv(path.join(organism, "stats.csv"))
     return df
 
-
 def describe(df):
     print(df.describe())
+
+def N_count(df, N_count=200):
+    N_count = df[df["N Count"] < N_count]
+    return N_count
+
+def contigs(df, contigs=200):
+    contigs = df[df["Contigs"] < contigs]
+    return contigs
 
 def Main():
     parser = argparse.ArgumentParser(description = "Functions to explore qc stats")
     parser.add_argument("organism", help="Path to the fasta dir you want to know about")
-    parser.add_argument("-d", "--describe", help="Summary stats.csv")
+    parser.add_argument("-d", "--describe", help="Summary stats.csv", action='store_true')
     args = parser.parse_args()
 
-    read_csv(args.organism)
+    df = read_csv(args.organism)
 
     if args.describe:
         describe(df)
+
+    elif args.N_count:
+        N_count(df)
+
     else:
         print("wtf")
 
