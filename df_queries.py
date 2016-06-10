f1fcc500644d45659a71cd7d5196e1d499e7645a
andrewsanchez <aas229@nau.edu>
2016-06-10 11:26:15 -0700

    updated

--- df_queries.py
+++ df_queries.py
@@ -6,10 +6,9 @@ from subprocess import call
 import argparse
 
 # make dirs to hold reorganize fastas into passed/failed groups
-def mkdirs():
-    cwd = getcwd()
-    pass_dir = path.join(cwd, "pass")
-    fail_dir = path.join(cwd, "fail")
+def mkdirs(organism):
+    pass_dir = path.join(organism, "pass")
+    fail_dir = path.join(organism, "fail")
 
     if not path.isdir(pass_dir): 
         mkdir(pass_dir)
@@ -18,12 +17,14 @@ def mkdirs():
 
 def read_csv(organism):
     df = pd.read_csv(path.join(organism, "stats.csv"))
+    print("Number of fastas in this directory:  {}".format(len(df)))
     return df
 
 def describe(df):
-    print("Summary of unfiltered fastas:")
+    print("Summary of fastas prior to filtering:")
     description = df.describe()
     print(description)
+    return description
 
 def N_count(df, N_count=200):
     N_count = df[df["N Count"] < N_count]
@@ -34,31 +35,52 @@ def contigs(df, contigs=200):
     return contigs
 
 def df_filters(df, fsize=5000000, contigs=500, N_count=500):
+    print("Enter the max values for number of N's, contigs, and file size.")
+    print("Press enter to use the default values file_size=5000000, contigs=500, N_count=500")
+    fsize = int(input("  > Max File size:  ") or "5000000")
+    contigs = int(input("  > Max contigs:  ") or "500")
+    N_count = int(input("  > Max N count:  ") or "500")
     df = df[ (df["File Size"] >= fsize) & (df["Contigs"] <= contigs) & (df["N Count"] <= N_count) ]
+    print("There are {} fastas passing the filter".format(len(df)))
+    again = input("Try again with different values?\n  > y/n:  ")
+    if again == "y":
+        dialogue(df)
+    else:
+        pass
     return df
 
-def dialogue():
-    print("There are {} files matching the filter".format(len(df)))
-    print("Summary of the unfiltered data frame:")
-    print(description)
-    print("Please enter the max values for N, contigs, and file size.")
-    print("Press enter at each prompt to use default values:\n\
-    N_count=500\n\
-    contigs=500\n\
-    file_size=5000000\n")
-    N_count = int(input("  > Max N count:  "))
-    contigs = int(input("  > Max N count:  "))
-    File_size = int(input("  > Max N count:  "))
+def dialogue(df):
+    print("Enter the max values for number of N's, contigs, and file size.")
+    print("Press enter to use the default values file_size=5000000, contigs=500, N_count=500")
+    fsize = int(input("  > Max File size:  ") or "5000000")
+    contigs = int(input("  > Max contigs:  ") or "500")
+    N_count = int(input("  > Max N count:  ") or "500")
+    df = df_filters(df, fsize=fsize, contigs=contigs, N_count=N_count)
+    print("There are {} fastas passing the filter".format(len(df)))
+    again = input("Try again with different values?\n  > y/n:  ")
+    if again == "y":
+        dialogue(df)
+    else:
+        pass
+    return df
 
 #def mash(reference):
     # subprocess(path to mash executable)
     # make sketch, etc.
 
-#def sort_files(df, organism):
-#   for f in df["Accession"]:
-#       source = join(organism, f)
-#       dest = join(organism, "pass", f)
-#       call.([ "ln", "-s", source, dest ])
+def sort_files(df, organism):
+    organize = input("Organize files based on these parameters?\n  > y/n:  ")
+    if organize == "y":
+        for f in df["Accession"]:
+            source = path.join(organism, f)
+            dest = path.join(organism, "pass", f)
+            call([ "ln", "-s", source, dest ])
+        print("Fastas that passed the quality control filters are now linked in the folder 'pass'")
+        print("Fastas that failed are linked in the folder 'fail'")
+    elif organize == "n":
+        print("Fastas were not reorganized.  Ciao!")
+    else:
+        print("Please answer y or n")
 
 
 def Main():
@@ -71,22 +93,14 @@ def Main():
     args = parser.parse_args()
 
     organism = args.organism
+    mkdirs(organism)
     df = read_csv(organism)
-    describe(df)
     description = describe(df)
-
     std_dict = {i:description[i]["std"] for i in description.columns}
     mean_dict = {i:description[i]["mean"] for i in description.columns}
     min_dict = {i:description[i]["min"] for i in description.columns}
     max_dict = {i:description[i]["max"] for i in description.columns}
-
-    """
-
-    
-
-    """
-
-    df = df_filters(df, fsize=args.file_size, contigs=args.contigs, N_count=args.N_count)
-    print("Files matching this criteria:  {}".format(len(df)))
+    df = df_filters(df)
+    sort_files(df, organism)
 
 Main()
