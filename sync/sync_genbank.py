439834f25e4cd999b6fe6157c910aa4f2cbe2ab2
andrewsanchez <aas229@nau.edu>
2016-11-16 13:41:18 -0700

    update

--- sync/sync_genbank.py
+++ sync/sync_genbank.py
@@ -7,6 +7,7 @@ from urllib.request import urlretrieve
 from ftplib import FTP, error_temp
 from urllib.error import URLError
 from time import strftime, sleep
+from glob import glob
 from re import sub
 
 def get_assembly_summary(genbank_mirror, assembly_summary_url="ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt"):
@@ -115,18 +116,18 @@ def check_species_dirs(genbank_mirror, species):
 
     return species_dir
 
-def grab_zipped_genome(genbank_mirror, species, genome_url, genome_id, ext=".fna.gz"):
+def grab_zipped_genome(genbank_mirror, species, genome_id, genome_path, ext=".fna.gz"):
 
     """
     Download compressed genome from ftp://ftp.ncbi.nlm.nih.gov/genomes/all/
     """
 
-    zipped = "{}_genomic{}".format(genome_url, ext)
-    zipped_dst = "{}_genomic.fasta".format(genome_id)
-    zipped_url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/{}/{}".format(genome_url, zipped)
-    zipped_dst = os.path.join(genbank_mirror, species, zipped)
+    zipped = "{}_genomic{}".format(genome_path, ext)
+    zipped_dst = "{}_genomic{}".format(genome_id, ext)
+    zipped_url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/{}/{}".format(genome_path, zipped)
+    zipped_dst = os.path.join(genbank_mirror, species, zipped_dst)
+    zipped_src = zipped_dst
     urlretrieve(zipped_url, zipped_dst)
-    zipped_src = os.path.join(genbank_mirror, species, zipped)
 
     return zipped_src
 
@@ -146,25 +147,27 @@ def unzip_genome(genbank_mirror, zipped_src, species, genome_id):
     zipped.close()
     os.remove(zipped_src)
 
-def check_local_genomes(local_genomes, latest_genomes, species, genome_id, genbank_stats):
+def check_local_genomes(genbank_mirror, species, local_genome_ids, latest_genome_ids, genbank_stats):
 
-    for genome_id in local_genomes:
-        if genome_id not in latest_genomes:
-            os.remove(os.path.join(genbank_mirror, species, "{}_genomic.fasta".format(genome_id)))
-            with open(genbank_stats, "a") as stats:
-                stats.write("{} removed\n".format(genome_id))
+    with open(genbank_stats, "a") as stats:
+        for genome_id in local_genome_ids:
+            if genome_id not in latest_genome_ids:
+                fasta = glob("{}*".format(genome_id))
+                os.remove(os.path.join(genbank_mirror, species, fasta[0]))
+                stats.write("{} removed\n".format(fasta[0]))
 
-def sync_latest_genomes(local_genomes, latest_genome_info, species, genome_id, genbank_stats):
+def sync_latest_genomes(genbank_mirror, species, local_genome_ids, ids_and_paths, genbank_stats):
 
-    for info in latest_genomes:
+    for info in ids_and_paths:
         genome_id = info[0]
-        genome_url = info[1]
-        if genome_id not in local_genomes:
+        genome_path = info[1]
+        print(genome_id, genome_path)
+        if genome_id not in local_genome_ids:
             try:
-                zipped_src = grab_zipped_genome(genbank_mirror, species, genome_url)
+                zipped_src = grab_zipped_genome(genbank_mirror, species, genome_id, genome_path)
                 unzip_genome(genbank_mirror, zipped_src, species, genome_id)
             except URLError:
-                zipped_src = grab_zipped_genome(genbank_mirror, species, genome_url, ext=".fasta.gz")
+                zipped_src = grab_zipped_genome(genbank_mirror, species, genome_id, genome_path, ext=".fasta.gz")
                 unzip_genome(genbank_mirror, zipped_src, species, genome_id)
             except URLError:
                 with open(genbank_stats, "a") as stats:
@@ -178,16 +181,17 @@ def grab_and_organize_genomes(genbank_mirror, genbank_stats, latest_assembly_ver
 
     for species in species_directories:
         species_dir = check_species_dirs(genbank_mirror, species)
-        local_genomes = ["_".join(genome_id.split("_")[:2]) for genome_id in os.listdir(species_dir)]
+        local_genome_ids = ["_".join(genome_id.split("_")[:2]) for genome_id in os.listdir(species_dir)]
         latest_genome_ids = [sub("[\[\]']", "", str(i)) for i in latest_assembly_versions.loc[species, ["id"]].values.tolist()]
-        latest_genome_dirs = [sub("[\[\]']", "", str(i)) for i in latest_assembly_versions.loc[species, ["dir"]].values.tolist()]
-        latest_genome_info = zip(latest_genome_ids, latest_genome_dirs)
+        latest_genome_paths = [sub("[\[\]']", "", str(i)) for i in latest_assembly_versions.loc[species, ["dir"]].values.tolist()]
+        ids_and_paths = zip(latest_genome_ids, latest_genome_paths)
         print("species", species)
-        print("local_genomes", local_genomes)
-        print("latest_genomes", latest_genomes)
+        print("local_genome_ids", local_genome_ids)
+        print("latest_genome_ids", latest_genome_ids)
+        print("latest_genome_paths", latest_genome_paths)
 
-        check_local_genomes(local_genomes, latest_genomes, species, genome_id, genbank_stats)
-        sync_latest_genomes(local_genomes, latest_genomes, latest_genome_dirs, species, genome_id, genbank_stats)
+        check_local_genomes(genbank_mirror, species, local_genome_ids, latest_genome_ids, genbank_stats)
+        sync_latest_genomes(genbank_mirror, species, local_genome_ids, ids_and_paths, genbank_stats)
 
 def log_error(msg, species, genbank_stats, ymdt):
     if msg == "latest":
@@ -202,15 +206,16 @@ def main():
     args = parser.parse_args()
 
     genbank_mirror = args.genbank_mirror
+    assembly_summary = get_assembly_summary(genbank_mirror, assembly_summary_url="ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt")
     ymdt = strftime("%y.%m.%d_%H%M")
     genbank_stats = os.path.join(genbank_mirror, ".info", "genbank_stats_{}.txt".format(ymdt))
-    check_dirs(genbank_mirror)
-    complete_species_list = ftp_complete_species_list()
-    latest_assembly_versions = get_latest_assembly_versions(genbank_mirror, complete_species_list, genbank_stats, ymdt)
-#   latest_assembly_versions = os.path.join(genbank_mirror, ".info", "latest_assembly_versions.csv")
-#   latest_assembly_versions = pd.read_csv(latest_assembly_versions, index_col=0, header=None)
-#   latest_assembly_versions.columns = ["id"]
-#   grab_and_organize_genomes(genbank_mirror, genbank_stats, latest_assembly_versions)
-#   rename(genbank_mirror, assembly_summary)
+#   check_dirs(genbank_mirror)
+#   complete_species_list = ftp_complete_species_list()
+#   latest_assembly_versions = get_latest_assembly_versions(genbank_mirror, complete_species_list, genbank_stats, ymdt)
+    latest_assembly_versions = os.path.join(genbank_mirror, ".info", "latest_assembly_versions.csv")
+    latest_assembly_versions = pd.read_csv(latest_assembly_versions, index_col=0, header=None)
+    latest_assembly_versions.columns = ["id", "dir"]
+    grab_and_organize_genomes(genbank_mirror, genbank_stats, latest_assembly_versions)
+    rename(genbank_mirror, assembly_summary)
 
 main()
