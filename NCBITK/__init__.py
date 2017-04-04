ac877f9f577f62cb351cdfa22c65d3f93407d4fd
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-04 09:57:41 -0700

    Include more modules in import to use in run.py

--- NCBITK/__init__.py
+++ NCBITK/__init__.py
@@ -1,4 +1,6 @@
 import NCBITK.config as config
+import NCBITK.rename as rename
 import NCBITK.sync as sync
 import NCBITK.curate as curate
 import NCBITK.get_resources as get_resources
+import NCBITK.filtering.mash as mash
