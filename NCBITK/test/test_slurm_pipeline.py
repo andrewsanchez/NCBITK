89826daabd286c030eb41af1b51775e99e446a02
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-06 12:55:55 -0700

    Clean up and reorganize
    Check if directories already exist to avoid error.
    Add a few more tests.

--- NCBITK/test/test_slurm_pipeline.py
+++ NCBITK/test/test_slurm_pipeline.py
@@ -15,20 +15,28 @@ class TestSlurmPipeline(unittest.TestCase):
         # self.genbank_mirror = tempfile.mkdtemp()
         self.genbank_mirror = '/scratch/aas229/test'
         self.info_dir = os.path.join(self.genbank_mirror, ".info")
-        os.mkdir(self.genbank_mirror)
-        os.mkdir(self.info_dir)
+
+        if not os.path.isdir(self.genbank_mirror):
+            os.mkdir(self.genbank_mirror)
+        if not os.path.isdir(self.info_dir):
+            os.mkdir(self.info_dir)
+
         assembly_summary_src = 'NCBITK/test/resources/assembly_summary.txt'
-        shutil.copyfile(assembly_summary_src, os.path.join(self.info_dir, 'assembly_summary.txt'))
+        assembly_summary_dst = os.path.join(self.info_dir, 'assembly_summary.txt')
+        shutil.copyfile(assembly_summary_src, assembly_summary_dst)
+
         self.specification = 'NCBITK/test/specification.json'
         self.assembly_summary = pd.read_csv('NCBITK/test/resources/assembly_summary.txt', sep="\t", index_col=0)
         self.species_list = 'Acinetobacter_nosocomialis'
+        self.species_dir = os.path.join(self.genbank_mirror, self.species_list)
 
     def test_slurm_pipeline(self):
 
         self.cmd = "slurm-pipeline.py -s {} {}".format(self.specification, self.genbank_mirror)
         subprocess.Popen(self.cmd, shell=True).wait()
-        # os.listdir(os.path.join(self.genbank_mirror, self.species_list))
-        self.assertTrue(os.listdir(self.genbank_mirror))
+        self.assertTrue(os.path.isdir(self.genbank_mirror))
+        self.assertTrue(os.path.isdir(self.info_dir))
+        self.assertTrue(os.path.isdir(self.species_dir))
 
     # def test_assess_genbank_mirror(self):
 
