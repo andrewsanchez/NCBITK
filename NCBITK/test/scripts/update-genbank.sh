266fcf2149f929d8e826a34701f6579a2e2c67e4
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-06 12:54:34 -0700

    remove commented line

--- NCBITK/test/scripts/update-genbank.sh
+++ NCBITK/test/scripts/update-genbank.sh
@@ -2,4 +2,3 @@
 
 species='Acinetobacter_nosocomialis'
 python ~/projects/NCBITK/run.py --update "$@" --use_local --species $species
-# python ~/projects/NCBITK/NCBITK/filtering/run.py -p "$@" --use_local --species $species
