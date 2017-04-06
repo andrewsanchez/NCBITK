9ca7f29df52eb9d4fd1504c868eb0569ebb9c4de
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-06 12:05:36 -0700

    Update

--- NCBITK/test/scripts/mash.sh
+++ NCBITK/test/scripts/mash.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+
+species='Acinetobacter_nosocomialis'
+python ~/projects/NCBITK/run.py --mash /scratch/aas229/test --use_local --species $species
--- NCBITK/test/scripts/update-genbank.sh
+++ NCBITK/test/scripts/update-genbank.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pwd
-mkdir -p "$@"/.info
-cp NCBITK/test/resources/assembly_summary.txt "$@"/.info/assembly_summary.txt
-python ~/projects/NCBITK/run.py "$@" --use_local --species Acinetobacter_nosocomialis
-ls -alR "$@"
+species='Acinetobacter_nosocomialis'
+python ~/projects/NCBITK/run.py --update "$@" --use_local --species $species
+# python ~/projects/NCBITK/NCBITK/filtering/run.py -p "$@" --use_local --species $species
