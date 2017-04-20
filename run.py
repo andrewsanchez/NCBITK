6f3b9599308c3386fa5511cf254bf4e61db2e351
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-20 13:25:10 -0700

    Run rysnc from CL and try to make options more sensible

--- run.py
+++ run.py
@@ -15,7 +15,6 @@ def setup(genbank_mirror, species_list, update_assembly_summary):
     path_vars = config.instantiate_path_vars(genbank_mirror)
     info_dir, slurm, out, logger = path_vars
     assembly_summary = get_resources.get_resources(genbank_mirror, logger, update_assembly_summary)
-    logger.info('{} genomes in assembly_summary.txt'.format(len(assembly_summary)))
     species = curate.get_species_list(assembly_summary, species_list)
 
     return path_vars, assembly_summary, species
@@ -52,17 +51,28 @@ def main():
     parser.add_argument("genbank_mirror", help = "Directory to save fastas", type=str)
     parser.add_argument("-s", "--species", help = 'List of species', nargs='+', default='all')
     parser.add_argument("-p", "--slurm", help = 'Submit jobs in parallel via SLURM pipeline tool.', action="store_true")
-    parser.add_argument("-u", "--update", action="store_true")
-    parser.add_argument("--update_assembly_summary", help = 'Use local copy new assembly_summary.txt and names.dmp', action="store_true", default=True)
+    parser.add_argument("--no_update", action="store_false")
+    parser.add_argument("-r", "--rsync", action="store_true")
+    parser.add_argument("--use_local_assembly", help = 'Use local copy new assembly_summary.txt and names.dmp',\
+                        action="store_true")
     args = parser.parse_args()
 
+    update_assembly_summary=True
+    if args.use_local_assembly:
+        update_assembly_summary=False
+
     genbank_mirror = args.genbank_mirror
-    path_vars, assembly_summary, species = setup(genbank_mirror, args.species, args.update_assembly_summary)
+    path_vars, assembly_summary, species = setup(genbank_mirror, args.species, update_assembly_summary)
     info_dir, slurm, out, logger = path_vars
     genbank_status = assess_genbank(genbank_mirror, assembly_summary, species, logger)
+    local_genomes, new_genomes, old_genomes, sketch_files, missing_sketch_files = genbank_status
 
-    if args.update:
-        update(genbank_mirror, genbank_status, path_vars, assembly_summary, species)
+    if args.rsync:
+        curate.create_species_dirs(genbank_mirror, assembly_summary, logger, species)
+        curate.remove_old_genomes(genbank_mirror, assembly_summary, old_genomes, logger)
+        sync.rsync_latest_genomes(genbank_mirror, assembly_summary, new_genomes)
+        curate.unzip_genbank_mirror(genbank_mirror)
+        rename.rename(genbank_mirror, assembly_summary)
 
 if __name__ == "__main__":
     main()
