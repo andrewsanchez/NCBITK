e450b00ba5066a98730cdfc49c2da2fb4bb9f305
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-20 13:23:29 -0700

    Implement rsync functionality again
    Create function for writing a file that contains a list of the full paths for
    all new genomes.  Create function to run rsync with that list as input.

--- NCBITK/sync.py
+++ NCBITK/sync.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import os
+import re
 import argparse
+import subprocess
+
 from urllib.request import urlretrieve
 from urllib.error import URLError
 from ftplib import error_temp
@@ -45,6 +48,37 @@ def sync_latest_genomes(genbank_mirror, assembly_summary, new_genomes, logger):
         except URLError as e:
             logger.exception('URLError[2] for {}\n{}'.format(genome_id, e))
             continue
+
+def write_ftp_paths(genbank_mirror, assembly_summary, new_genomes):
+
+    ftp_paths_file = os.path.join(genbank_mirror, '.info', 'ftp_paths.txt')
+
+    if os.path.isfile(ftp_paths_file):
+        os.remove(ftp_paths_file)
+
+    with open(ftp_paths_file, 'a') as f:
+        for accession in new_genomes:
+            genome_url = assembly_summary.ftp_path[accession]
+            genome_url = re.sub(r'ftp://ftp.ncbi.nlm.nih.gov/genomes/all/', '', genome_url)
+            genome_parent_dir = genome_url.split('/')[-1]
+            genome_url = '{}/{}_genomic.fna.gz'.format(genome_url, genome_parent_dir)
+            f.write(genome_url)
+            f.write('\n')
+
+    return ftp_paths_file
+
+def rsync_latest_genomes(genbank_mirror, assembly_summary, new_genomes):
+
+    ftp_paths_file = write_ftp_paths(genbank_mirror, assembly_summary, new_genomes)
+    rsync_log = os.path.join(genbank_mirror, '.info', 'rsync_{}.out'.format(strftime('%Y.%m.%d.%H:%M')))
+    incoming = os.path.join(genbank_mirror, 'incoming')
+    if not os.path.isdir(incoming):
+        os.mkdir(incoming)
+
+    cmd = 'rsync --chmod=ugo=rwX --times --progress --itemize-changes --stats --files-from={}\
+    --log-file={} --prune-empty-dirs ftp.ncbi.nlm.nih.gov::genomes/all/ {}'.format(ftp_paths_file, rsync_log, incoming)
+
+    subprocess.Popen(cmd, shell=True).wait()
 
 def main():
 
