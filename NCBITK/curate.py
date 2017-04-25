15f1097962ec97e38ae46248f0ab8e6336f27588
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-04-25 12:03:56 -0700

    Move the renaming functions into curate.py
    Unnecessary for it to be in it's own module

--- NCBITK/curate.py
+++ NCBITK/curate.py
@@ -4,6 +4,9 @@ import glob
 import gzip
 import re
 import logging
+import shutil
+import pandas as pd
+
 
 def clean_up(genbank_mirror, path_vars):
 
@@ -167,6 +170,54 @@ def post_rsync_cleanup(genbank_mirror, assembly_summary, logger):
 
     # shutil.rmtree(incoming)
 
+
+def rename(target_dir, assembly_summary):
+
+    """
+    Clean up assembly_summary.txt and renamed FASTA's.
+    """
+
+    def rm_duplicates(seq):
+
+        """
+        remove duplicate strings during renaming
+        """
+
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
+
+    # If infraspecific_name and isolate columns are empty, fill infraspecific_name with "NA"
+    assembly_summary.update(assembly_summary['infraspecific_name'][(assembly_summary['infraspecific_name'].isnull()) &\
+            (assembly_summary['isolate'].isnull())].fillna('NA'))
+
+    # If infraspecific_name column is empty and isolate column is not empty, fill infraspecific_name with the value of isolate.
+    assembly_summary.update(assembly_summary['infraspecific_name'][(assembly_summary['infraspecific_name'].isnull()) &\
+            (assembly_summary['isolate'].notnull())].fillna(assembly_summary['isolate']))
+
+    assembly_summary.assembly_level.replace({' ': '_'}, regex=True, inplace=True)
+    assembly_summary.organism_name.replace({' ': '_'}, regex=True, inplace=True)
+    assembly_summary.organism_name.replace({'[\W]': '_'}, regex=True, inplace=True)
+    assembly_summary.infraspecific_name.replace({'[\W]': '_'}, regex=True, inplace=True)
+
+    for root, dirs, files in os.walk(target_dir):
+        for f in files:
+            if f.startswith("GCA"):
+                accession_id = '_'.join(f.split('_') [:2])
+                if accession_id in assembly_summary.index:
+                    org_name = assembly_summary.get_value(accession_id, 'scientific_name')
+                    strain = assembly_summary.get_value(accession_id, 'infraspecific_name')
+                    assembly_level  = assembly_summary.get_value(accession_id, 'assembly_level')
+                    new_name = '{}_{}_{}_{}.fasta'.format(accession_id, org_name, strain, assembly_level)
+                    rm_words = re.compile( r'((?<=_)(sp|sub|substr|subsp|str|strain)(?=_))' )
+                    new_name = rm_words.sub('_', new_name)
+                    new_name = re.sub(r'_+', '_', new_name )
+                    new_name = new_name.split('_')
+                    new_name = rm_duplicates(new_name)
+                    new_name = '_'.join(new_name)
+                    old = os.path.join(root, f)
+                    new = os.path.join(root, new_name)
+                    os.rename(old, new)
 
 def main():
 
