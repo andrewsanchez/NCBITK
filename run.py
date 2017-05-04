fd811d719da7c10a85b5901e4cb708553b3f72a6
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-05-04 11:10:43 -0700

    Add --status flag and corresponding fun.
     Also some trivial clean up and improve cl logic

--- run.py
+++ run.py
@@ -32,33 +32,61 @@ def update(genbank_mirror, genbank_status, path_vars, assembly_summary, species_
     curate.unzip_genbank_mirror(genbank_mirror)
     rename.rename(genbank_mirror, assembly_summary)
 
+def show_genbank_status(genbank_status):
+
+    local_genomes, new_genomes, old_genomes = genbank_status
+    print('{} local genome(s)'.format(len(local_genomes)))
+    print('{} new genome(s)'.format(len(new_genomes)))
+    print('{} old genome(s)'.format(len(old_genomes)))
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("genbank_mirror", help = "Directory to save fastas", type=str)
-    parser.add_argument("-s", "--species", help = 'List of species', nargs='+', default='all')
-    parser.add_argument("-p", "--parallel", help = 'Submit jobs in parallel via SLURM pipeline tool.', action="store_true")
-    parser.add_argument("-r", "--rsync", action="store_true")
-    parser.add_argument("--use_local_assembly", help = 'Use local copy new assembly_summary.txt and names.dmp',\
+    parser.add_argument("genbank_mirror",
+                        help = "Directory to save fastas",
+                        type=str)
+    parser.add_argument("-s", "--species",
+                        help = 'List of species',
+                        nargs='+', default='all')
+    parser.add_argument("-p", "--parallel",
+                        help = 'Submit jobs in parallel via SLURM pipeline tool.',
+                        action="store_true")
+    parser.add_argument("-u", "--update",
+                        action="store_true")
+    parser.add_argument("--use_local_assembly",
+                        help = 'Use local copy new assembly_summary.txt and names.dmp',
+                        action="store_true")
+    parser.add_argument("--status",
+                        help = 'Show the current status of your local copy of genbank.',
                         action="store_true")
     args = parser.parse_args()
 
-    update_assembly_summary=True
     if args.use_local_assembly:
         update_assembly_summary=False
+    else:
+        update_assembly_summary=True
 
     genbank_mirror = args.genbank_mirror
-    path_vars, assembly_summary, species = setup(genbank_mirror, args.species, update_assembly_summary)
+
+    path_vars, assembly_summary, species, genbank_status = setup(genbank_mirror,
+                                                 args.species,
+                                                 update_assembly_summary)
     info_dir, slurm, out, logger = path_vars
-    genbank_status = curate.assess_genbank_mirror(genbank_mirror, assembly_summary, species, logger)
     local_genomes, new_genomes, old_genomes = genbank_status
 
-    curate.create_species_dirs(genbank_mirror, logger, species)
-    curate.remove_old_genomes(genbank_mirror, assembly_summary, local_genomes, old_genomes, logger)
-    sync.rsync_latest_genomes(genbank_mirror, assembly_summary, new_genomes)
-    post_rsync_cleanup(genbank_mirror, assembly_summary, logger)
-    curate.unzip_genbank_mirror(genbank_mirror)
-    rename.rename(genbank_mirror, assembly_summary)
+    if args.status:
+        show_genbank_status(genbank_status)
+
+    if args.update:
+        curate.create_species_dirs(genbank_mirror, logger, species)
+        curate.remove_old_genomes(genbank_mirror,
+                                  assembly_summary,
+                                  local_genomes,
+                                  old_genomes,
+                                  logger)
+        sync.rsync_latest_genomes(genbank_mirror, assembly_summary, new_genomes)
+        curate.post_rsync_cleanup(genbank_mirror, assembly_summary, logger)
+        curate.unzip_genbank_mirror(genbank_mirror)
+        curate.rename(genbank_mirror, assembly_summary)
 
 if __name__ == "__main__":
     main()
