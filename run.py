971796b559a83e403b5fd7b0ed285d5ecdcfe261
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-05-17 11:59:16 -0700

    Mirror changes in __main__.py

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 import argparse
 import logging
@@ -41,23 +39,20 @@ def show_genbank_status(genbank_status):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("genbank_mirror",
-                        help = "Directory to save fastas",
+    parser.add_argument('genbank_mirror',
+                        help='Directory to save genomes to',
                         type=str)
-    parser.add_argument("-s", "--species",
-                        help = 'List of species',
+    parser.add_argument('-s', '--species',
+                        help='List of species to download genomes for',
                         nargs='+', default='all')
-    parser.add_argument("-p", "--parallel",
-                        help = 'Submit jobs in parallel via SLURM pipeline tool.',
-                        action="store_true")
-    parser.add_argument("-u", "--update",
-                        action="store_true")
-    parser.add_argument("--use_local_assembly",
-                        help = 'Use local copy new assembly_summary.txt and names.dmp',
-                        action="store_true")
-    parser.add_argument("--status",
-                        help = 'Show the current status of your local copy of genbank.',
-                        action="store_true")
+    parser.add_argument('-u', '--update',
+                        action='store_true')
+    parser.add_argument('--use_local_assembly',
+                        help='Use local assembly_summary and taxonomy dump',
+                        action='store_true')
+    parser.add_argument('--status',
+                        help='Show the current status of your genome collection',
+                        action='store_true')
     args = parser.parse_args()
 
     if args.use_local_assembly:
@@ -66,10 +61,9 @@ def main():
         update_assembly_summary=True
 
     genbank_mirror = args.genbank_mirror
-
     path_vars, assembly_summary, species, genbank_status = setup(genbank_mirror,
-                                                 args.species,
-                                                 update_assembly_summary)
+                                                                 args.species,
+                                                                 update_assembly_summary)
     info_dir, slurm, out, logger = path_vars
     local_genomes, new_genomes, old_genomes = genbank_status
 
